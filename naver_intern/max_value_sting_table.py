e8f77ea0f0d04e2199d8daa2c8ef7f6be2acb838
kona419 <74047746+kona419@users.noreply.github.com>
2022-07-06 00:00:00 +0900

    Update max_value_sting_table.py

--- naver_intern/max_value_sting_table.py
+++ naver_intern/max_value_sting_table.py
@@ -1,1 +1,16 @@
+def solution(s, c):
+    lines = s.split('\n')
+    data_values = []
+    header = lines[0].split(",")
+    header_index = header.index(c)
+    for lines in lines[1:]:
+        data = lines.split(",")
+        data_values.append(data[header_index])
+        data_values = list(map(int,data_values))
+    return max(data_values)
 
+
+S = "id,name,age,act.,room,dep.\n1,Jack,68,T,13,8\n17,Betty,28,F,15,7"
+C = "age"
+result = solution(S, C)
+print(result)
